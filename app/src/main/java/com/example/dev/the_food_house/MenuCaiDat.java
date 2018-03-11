0f97bde0b93fbe9c0f71c97431bbaae57e128897
Ðoàn Ng?c Vuong <kiodark2@gmail.com>
2018-03-11 17:49:18 +0700

    Đã commit phần Setting.

--- app/src/main/java/com/example/dev/the_food_house/MenuCaiDat.java
+++ app/src/main/java/com/example/dev/the_food_house/MenuCaiDat.java
@@ -8,23 +8,23 @@ public class MenuCaiDat {
     private String Ten;
     private int Hinh;
 
-    private MenuCaiDat(String ten, int hinh) {
+    public MenuCaiDat(String ten, int hinh) {
         Ten = ten;
         Hinh = hinh;
     }
-    private String getTen() {
+    public String getTen() {
         return Ten;
     }
 
-    private void setTen(String ten) {
+    public void setTen(String ten) {
         Ten = ten;
     }
 
-    private int getHinh() {
+    public int getHinh() {
         return Hinh;
     }
 
-    private void setHinh(int hinh) {
+    public void setHinh(int hinh) {
         Hinh = hinh;
     }
 }

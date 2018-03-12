4584c79d667cfc9f66096b04f57ef7bf463f2eed
nhs192 <hoanson19296@gmail.com>
2018-03-12 10:06:30 +0700

    ..

--- app/src/main/java/com/example/dev/the_food_house/Food_Fragment.java
+++ app/src/main/java/com/example/dev/the_food_house/Food_Fragment.java
@@ -1,23 +1,58 @@
 package com.example.dev.the_food_house;
 
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-/**
- * Created by dev on 3/4/2018.
- */
+import java.util.ArrayList;
 
 public class Food_Fragment extends Fragment {
-    private View mRootView;
+
+    ListView lvMonAn;
+    ArrayList<String> arrayFood;
+    public View mRootView;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        mRootView = inflater.inflate(R.layout.fragment_food,container,false);
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_food);
+        lvMonAn = (ListView) container.findViewById(R.id.listviewMonAn);
+        mRootView = inflater.inflate(R.layout.fragment_food, container, false);
         return mRootView;
+    }
+
+    public void InitList(){
+
+
+        arrayFood = new ArrayList<>();
+
+        arrayFood.add("Thịt kho hột vịt");
+        arrayFood.add("Cơm chiên dương châu");
+        arrayFood.add("Rau muống xào tỏi");
+        arrayFood.add("Gà xào măng");
+        arrayFood.add("Canh chua cá lóc");
+        arrayFood.add("Thịt kho tiêu");
+        arrayFood.add("Thịt luộc mắm tôm");
+        arrayFood.add("Tàu hủ dồn thịt");
+        arrayFood.add("Gà chiên nướng mắm");
+        arrayFood.add("Cơm chiên csa mặn");
+        arrayFood.add("Mì vịt tiềm");
+        arrayFood.add("Mì xào dòn");
+        arrayFood.add("Cá chiên");
+        arrayFood.add("Cơm cháy chiên dòn");
+
+        ArrayAdapter adapter = new ArrayAdapter(this.getContext(), android.R.layout.simple_list_item_1, arrayFood);
+
+        lvMonAn.setAdapter(adapter);
     }
 }
